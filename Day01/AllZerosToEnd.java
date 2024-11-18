1e9f58d25bf0699b5735ca0b39d220d4acdbaa19
Ujjawal Pratap Singh <129609821+Ujjawalps@users.noreply.github.com>
2024-11-18 23:36:09 +0530

    Solved All Zeros to End Problem

---
+++ Day01/AllZerosToEnd.java
@@ -0,0 +1,30 @@
+package GFG160.Day01;
+
+import java.util.Arrays;
+
+public class AllZerosToEnd {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 0, 4, 3, 0, 5, 0};
+        int[] arr2 = {10, 20, 30};
+        int[] arr3 = {0, 0};
+        pushZerosToEnd(arr1);
+        System.out.println(Arrays.toString(arr1));
+        pushZerosToEnd(arr2);
+        System.out.println(Arrays.toString(arr2));
+        pushZerosToEnd(arr3);
+        System.out.println(Arrays.toString(arr3));
+    }
+    static void pushZerosToEnd(int[] arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+
+                count++;
+            }
+        }
+    }
+}
