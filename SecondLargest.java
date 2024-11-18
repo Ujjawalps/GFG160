da8a4fcc2fb847a583cb891e3395966763a0520b
Ujjawal Pratap Singh <129609821+Ujjawalps@users.noreply.github.com>
2024-11-18 23:01:35 +0530

    Solved Second largest Element Problem

---
+++ SecondLargest.java
@@ -0,0 +1,30 @@
+package GFG160;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr1 = {12, 35, 1, 10, 34, 1};
+        int[] arr2 = {10, 5, 10};
+        int[] arr3 = {10, 10, 10};
+        System.out.println(getSecondLargest(arr1));
+        System.out.println(getSecondLargest(arr2));
+        System.out.println(getSecondLargest(arr3));
+    }
+    static int getSecondLargest(int[] arr) {
+        // Code Here
+        int max = -1;
+        int second_max = -1;
+        for (int j : arr) {
+            if (max < j) {
+                max = j;
+            }
+        }
+        for (int j : arr) {
+            if (second_max < j && j != max) {
+                second_max = j;
+            }
+
+        }
+        return second_max;
+    }
+
+}
