afcca693f0dc485a580f854acff829875207ceeb
Ujjawal Pratap Singh <129609821+Ujjawalps@users.noreply.github.com>
2024-11-18 23:44:06 +0530

    Solved Reverse Array Easy problem

---
+++ Day01/ReverseTheArray_E.java
@@ -0,0 +1,29 @@
+package GFG160.Day01;
+
+import java.util.Arrays;
+
+public class ReverseTheArray_E {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 4, 3, 2, 6, 5};
+        int[] arr2 = {4, 5, 2};
+        int[] arr3 = {1};
+        reverseArray(arr1);
+        System.out.println(Arrays.toString(arr1));
+        reverseArray(arr2);
+        System.out.println(Arrays.toString(arr2));
+        reverseArray(arr3);
+        System.out.println(Arrays.toString(arr3));
+
+    }
+    static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length-1;
+        while (left<=right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
