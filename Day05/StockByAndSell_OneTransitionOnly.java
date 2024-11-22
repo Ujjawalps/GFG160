1114b543bfdb16155ac2493ea593400f2985f8e4
Ujjawal Pratap Singh <129609821+Ujjawalps@users.noreply.github.com>
2024-11-22 23:28:02 +0530

    Solved Stock Buy And Sell One Transition Only

---
+++ Day05/StockByAndSell_OneTransitionOnly.java
@@ -0,0 +1,20 @@
+package GFG160.Day05;
+
+public class StockByAndSell_OneTransitionOnly {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 6, 9, 11};
+        System.out.println(maximumProfit(arr));
+    }
+    static int maximumProfit(int prices[]) {
+        // Code here
+        int min = prices[0];
+        int profit = 0;
+        for (int price : prices) {
+            if (price < min)
+                min = price;
+            if (profit < price - min)
+                profit = price - min;
+        }
+        return profit;
+    }
+}
